e7066164abcef7581e7f8898889a83e38b3609b3
Mohan <98559494+mohsenKh75@users.noreply.github.com>
2023-10-24 23:23:53 +0330

    Add files via upload

---
+++ useScrollingDirection.ts
@@ -0,0 +1,48 @@
+import { RefObject, useEffect, useRef, useState } from 'react';
+import { SCROLL_DIRECTION } from '@/constants/scrollDirection';
+import { ScrollDirectionType } from '@/constants/scrollDirection';
+import { isServerSide } from '@/utils';
+
+export const useScrollDirection = (ref?: RefObject<HTMLDivElement | null>): ScrollDirectionType => {
+  const getScrollY = () => {
+    if (ref) {
+      return !isServerSide ? ref?.current?.scrollTop : 0;
+    }
+    return !isServerSide ? window.scrollY : 0;
+  };
+
+  const scrollYRef = useRef(getScrollY());
+  const [scrollDirection, setScrollDirection] = useState<ScrollDirectionType>(SCROLL_DIRECTION.NONE);
+  useEffect(() => {
+    if (isServerSide) {
+      return undefined;
+    }
+
+    function handleScroll() {
+      const newY = getScrollY();
+      if (newY) {
+        if (scrollYRef.current && scrollYRef.current !== newY) {
+          setScrollDirection(scrollYRef.current > newY ? SCROLL_DIRECTION.UP : SCROLL_DIRECTION.DOWN);
+        } else {
+          setScrollDirection(SCROLL_DIRECTION.NONE);
+        }
+      }
+
+      scrollYRef.current = newY;
+    }
+    if (ref) {
+      ref?.current?.addEventListener('scroll', handleScroll);
+    } else {
+      window.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      ref
+        ? ref?.current?.removeEventListener('scroll', handleScroll)
+        : window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  if (isServerSide) return SCROLL_DIRECTION.NONE;
+
+  return scrollDirection;
+};
